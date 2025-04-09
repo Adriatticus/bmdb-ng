1a14ae5c2f936df60e9df18a329ec06132a99993
Adriatticus <4CorderJ@gmail.com>
2025-04-09 13:47:37 -0400

    please

--- src/app/feature/movie/movie-list/movie-list.component.ts
+++ src/app/feature/movie/movie-list/movie-list.component.ts
@@ -14,12 +14,9 @@ export class MovieListComponent implements OnInit, OnDestroy {
   title: string = 'Movie-List';
   movies!: Movie[];
   subscription!: Subscription;
-<<<<<<< HEAD
   loggedInUser!: User;
-=======
   sortOrder: string = 'asc';
   sortCriteria: string = 'id';
->>>>>>> 67269438e980c74b188794b89b5c8e70cc6c0c69
 
   constructor(private movieSvc: MovieService, private sysSvc: SystemService) {}
 
